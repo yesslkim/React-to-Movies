93e763df657c7f1d8326a82a460ebfb95a76609b
yesslkim <yskim940914@gmail.com>
2020-11-17 17:07:41 +0900

    add fetched data and send it to component

---
+++ react-to-movies/src/App.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import Movie from './components/movie';
+const MOVIE_API = 'https://yts.mx/api/v2/list_movies.json';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    fetch(MOVIE_API)
+    .then(res => res.json())
+    .then(data => {
+      setMovies(data.data.movies);
+    });
+  },[])
+  return (
+    <div className="movie-container">
+      {movies.map(movie => (
+        <Movie key={movie.id} {...movie}/>
+      ))}
+    </div>
+  )
+}
+
+export default App;
